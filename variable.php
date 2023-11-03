4f67c43b4d6105db8fb85cd326c2bea1d065c9c2
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-03 16:37:02 +0900

    add variable.php

---
+++ variable.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>PHPの基本</title>
+
+</head>
+<body>
+
+<?php
+$msg ='こんにちは、世界!';
+print $msg; // 結果：こんにちは、世界!
+?>
+
+</body>
+</html>
