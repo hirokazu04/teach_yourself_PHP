0f7c200c99f7498b38575b007adbb96615818037
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-11 16:30:42 +0900

    string.php 追加

---
+++ string.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 2.2</title>
+
+</head>
+<body>
+
+<?php
+$title ='サーバーサイド技術の学び舎 - WINGS';
+$data1 = "サポートサイト\t「{$title}」へ<br>";
+$data2 = 'サポートサイト\t「{$title}」へ<br>';
+print $data1;
+print $data2;
+
+
+?>
+
+</body>
+</html>
