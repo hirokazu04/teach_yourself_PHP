f7f47c75f4ddeb07a16f1e551a4d731b9929cc60
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-11 16:09:43 +0900

    ex2.2.php 追加

---
+++ ex2.2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 2.2</title>
+
+</head>
+<body>
+
+<?php
+print "現在実行中のファイル名= {__FILE__}  <br>";
+print "現在実行中の行番号= {__LINE__}  <br>";
+print "現在実行中のディレクトリ= {__DIR__}  <br>";
+
+?>
+
+</body>
+</html>
