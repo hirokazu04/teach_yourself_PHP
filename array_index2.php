15394db817672b311ae2b0d4a7d7deee671f7198
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-11 21:49:09 +0900

    a

---
+++ array_index2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 2.2</title>
+
+</head>
+<body>
+
+<pre>
+<?php
+$data =[1=> '山田', 4=> '掛谷', '日尾'];
+print_r($data);
+
+?>
+</pre>
+
+
+</body>
+</html>
