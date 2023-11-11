ca18541c7836c94cc68488472347aa25372386cf
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-11 22:06:40 +0900

    a

---
+++ ex2.4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 2.2</title>
+
+</head>
+<body>
+
+<pre>
+<?php
+$data =[ 1=> 10, 3=> 12, 5=>>5];
+$data[]=15;
+print_r($data);
+
+?>
+</pre>
+
+
+</body>
+</html>
