81a863fbd0978f82457009178503687e7320cc61
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-11 21:28:59 +0900

    heredoc.php 追加

---
+++ heredoc.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 2.2</title>
+
+</head>
+<body>
+
+<?php
+$str= 'PHP (PHP:Hypertext Preprocessor) ';
+$msg = <<<EOD
+{$str}は、サーバーサイドで動作するスクリプト言語です。
+まずは、本書でじっくり基礎固めしましょう。 <br/>
+"Let's start, everyone!!"
+EOD;
+print $msg;
+
+?>
+
+</body>
+</html>
