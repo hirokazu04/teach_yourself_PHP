6c10a5d09dc0031452784dca3297cffb392cabaa
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-12 13:53:22 +0900

    add cast.php

---
+++ cast.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>2.5.2 明示的な変換(キャスト)</title>
+
+</head>
+<body>
+
+<pre>
+
+<?php
+// 整数型へのキャスト
+var_dump((int)1530.95);
+var_dump((int)-1530.95);
+var_dump((int)true);
+var_dump((string)true);
+var_dump((int)false);
+var_dump((string)false);
+var_dump((int)array(108));
+
+
+
+?>
+
+
+</pre>
+
+
+</body>
+</html>
