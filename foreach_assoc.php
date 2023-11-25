177cd50ddaa3cb189e3f138db0f72cee8d3d7bd5
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-25 17:20:17 +0900

    add foreach_assoc.php

---
+++ foreach_assoc.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>4.2.5 </title>
+
+</head>
+<body>
+
+<pre>
+
+<?php
+
+$data = ['高江'=>'男', '掛谷'=>'女', '日尾'=>'男', '薄井'=>'女', '和田'=>'男'];
+foreach($data as $key=>$value){
+    print "{$key}:{$value}<br />";
+    
+}
+
+
+
+?>
+
+
+</pre>
+
+
+</body>
+</html>
