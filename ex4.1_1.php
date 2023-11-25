c77acb98bdc156e20b214af42bae2e535d408c89
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-25 16:56:35 +0900

    add ex4.1_1.php

---
+++ ex4.1_1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 4.1 (1) </title>
+
+</head>
+<body>
+
+<pre>
+
+<?php
+
+$result=75;
+if($result>=90){
+    print '優';
+}elseif($result>=70){
+    print '良';
+}elseif($result>=50){
+    print '可';
+}else{
+    print '不可';
+}
+
+
+?>
+
+
+</pre>
+
+
+</body>
+</html>
