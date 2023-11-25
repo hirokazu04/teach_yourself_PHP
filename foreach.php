d3585abf21a5bd4648224178e4a2f8094124e74f
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-11-25 17:05:15 +0900

    add foreach.php

---
+++ foreach.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>4.2.5 </title>
+
+</head>
+<body>
+
+<pre>
+
+<?php
+
+$data = ['高江', '掛谷', '日尾', '薄井', '和田'];
+foreach($data as $value){
+    print "名前は{$value}<br />";
+    
+}
+
+
+
+?>
+
+
+</pre>
+
+
+</body>
+</html>
