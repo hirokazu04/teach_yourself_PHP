4e52c18257a0a860768ee871e3debd619474ff46
Hirokazu Tago <hirotg_m@yahoo.co.jp>
2023-12-09 08:59:17 +0900

    add ex4.2_2.php

---
+++ ex4.2_2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>練習問題 4.1 (1) </title>
+
+</head>
+<body>
+
+<pre>
+
+<?php
+
+for($i=1; $i<=9; $i++){
+    for($j=1; $j<=9; $j++){
+        print $i * $j . " ";
+    }
+    print "<br/>";
+}
+
+
+?>
+
+
+</pre>
+
+
+</body>
+</html>
